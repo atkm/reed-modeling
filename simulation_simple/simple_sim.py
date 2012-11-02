80c9295cae78b48642b020ab56aa85d6e296d2ad
Wyatt Alt <wyaltster@gmail.com>
2012-11-02 01:29:09 -0700

    added wait lines to probablistic()

--- simulation_simple/simple_sim.py
+++ simulation_simple/simple_sim.py
@@ -1,6 +1,13 @@
 import sys
 import scipy as sp
 import numpy as np
+np.set_printoptions(precision = 5, suppress = True) 
+
+## *see corresponding update to the graph*
+##    - changed transition probabilities in Circle from 1/3 to 1/2, to reflect the graph.
+##	  - added rough waiting line functionality to probablistic().  It now takes a 
+##	    tuple (a,b,c) giving the number of cars lined up at each entrance.
+##      Traffic.enter() now 'loads' cars from the wait lines to the the wait line nodes.
 
 class Traffic:
     """
@@ -12,16 +19,16 @@ class Traffic:
         """
         self.size = len(distribution)
         self.wait_line = list(distribution)
-        self.state = np.zeros(3 * self.size)   
+        self.state = np.zeros(4*self.size)
     
     def enter(self):
         """
         If an entrance is empty, throw in a new car.
         """
         for i in range(self.size):
-            if (self.state[3*i] == 0) and (self.wait_line[i-1] > 0):
-                self.wait_line[i-1] -= 1
-                self.state[3*i] += 1
+            if (self.state[i] == 0) and (self.wait_line[i] > 0):
+                self.wait_line[i] -= 1
+                self.state[i] += 1
 
 
 class Car:
@@ -32,25 +39,28 @@ class Car:
 class Circle:
     def __init__(self):
         self.lanes = 1
-        # number of nodes = (eingang, ausgang, lane)*3 = 9
-        # order: E1, R1, A1, E2, R2, A2, E3, R3, A3
-        e1 = np.array([1,1,0,1,0,0,0,0,0])
-        r1 = np.array([0,1,1,1,0,0,0,0,0])
-        a1 = np.zeros(9)
-        e2 = np.array([0,0,0,1,1,0,1,0,0])
-        r2 = np.array([0,0,0,0,1,1,1,0,0])
-        a2 = np.zeros(9)
-        e3 = np.array([1,0,0,0,0,0,1,1,0])
-        r3 = np.array([1,0,0,0,0,0,0,1,1])
-        a3 = np.zeros(9)
+        # number of nodes = (eingang, ausgang, lane, waitline)*3 = 9
+        # order: W1, W2, W3, E1, R1, A1, E2, R2, A2, E3, R3, A3
+        w1 = np.array([0,0,0,2,0,0,0,0,0,0,0,0])
+        w2 = np.array([0,0,0,0,0,0,2,0,0,0,0,0])
+        w3 = np.array([0,0,0,0,0,0,0,0,0,2,0,0])
+        e1 = np.array([0,0,0,0,1,0,1,0,0,0,0,0])
+        r1 = np.array([0,0,0,0,0,1,1,0,0,0,0,0])
+        a1 = np.array([0,0,0,0,0,2,0,0,0,0,0,0])  #np.zeros(12)  
+        e2 = np.array([0,0,0,0,0,0,0,1,0,1,0,0])
+        r2 = np.array([0,0,0,0,0,0,0,0,1,1,0,0])
+        a2 = np.array([0,0,0,0,0,0,0,0,2,0,0,0]) #np.zeros(12)  
+        e3 = np.array([0,0,0,1,0,0,0,0,0,0,1,0])
+        r3 = np.array([0,0,0,1,0,0,0,0,0,0,0,1])
+        a3 = np.array([0,0,0,0,0,0,0,0,0,0,0,2]) #np.zeros(12)  
         # name these
-        nodes = [e1, r1, a1, e2, r2, a2, e3, r3, a3]
+        nodes = [w1, w2,w3, e1, r1, a1, e2, r2, a2, e3, r3, a3]
         # create Markov matrix
-        self.layout = np.vstack((v for v in nodes))/3
+        self.layout = np.vstack((v for v in nodes)).transpose()/2.
 
-def probablistic():
+def probablistic(dist = (1,1,1)):
     simple_circle = Circle()
-    simple_traffic = Traffic((1,1,1))
+    simple_traffic = Traffic(dist)
     print("Run Traffic.enter()")
     simple_traffic.enter()
     print("State:")
@@ -58,15 +68,16 @@ def probablistic():
     print("Wait line:")
     print(simple_traffic.wait_line)
     print("layout:")
-    print(simple_circle.layout)
-    
+    print(simple_circle.layout) 
     for i in range(100):
         print("Proceed one time step:")
         simple_traffic.state = np.dot(simple_circle.layout, simple_traffic.state)
+        simple_traffic.enter()
         print(simple_traffic.state)
-        if converged(simple_traffic.state):
+        if converged(np.array([simple_traffic.state[j] for j in [0,1,2,3,4,6,7,9,10]])):
             print("Result: " + repr(i) + " iterations.")
             break
+
 
 def converged(arr):
     if all(arr < 10**(-5)):
