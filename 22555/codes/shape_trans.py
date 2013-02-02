f437f7bdf4c783d21b10a7928c127e9a9df19ffa
Polytopia(trent) <akumano@reed.edu>
2013-02-01 20:10:41 -0800

    code to generate shapes

---
+++ 22555/codes/shape_trans.py
@@ -0,0 +1,62 @@
+import scipy as sp
+import numpy as np
+import scipy.optimize
+import scipy.fftpack
+
+def Henon(init, args):
+    """
+    args = (a,b)
+    init = (x0, y0)
+    Henon map 
+    f(x,y) = y + 1 - ax^n
+    g(x,y) = b*x
+    """
+    a = args[0]
+    b = args[1]
+    x0 = init[0]
+    y0 = init[1]
+    x = y0 + 1 - a*(x0**2)
+    y = b * x0
+    return (x,y)
+
+def Cat(init,args=()):
+    # actually, there's no arg
+    x0 = init[0]
+    y0 = init[1]
+    x  = sp.mod(2*x0 + y0, 1)
+    y  = sp.mod(x0 + y0, 1)
+    return (x,y)
+
+# pylab.plot(xaxis, graph) to plot
+# for example xasix = sp.linspace(0,1,100)
+
+def IterateList2D(g, init, N, args=()):
+    """
+    Iterate the function g(x, mu) N-1 times, starting at x0, so that the
+    full trajectory contains N points.
+    Returns the entire list 
+    (x, g(x), g(g(x)), ... g(g(...(g(x))...))). 
+
+    use
+        pylab.hist(attractorXs, bins=500, normed=1)
+        pylab.show()
+    to see the density of points.
+    """
+    x0 = init[0]
+    y0 = init[1]
+
+    result = [(x0, y0)]
+    for i in range(N-1):
+      result.append(g(result[-1],args))
+
+    # Return a numpy array
+    return np.array(result)
+
+#def PlotIterate2D(g, init, N, args=()):
+#    """
+#    Plots g, the diagonal y=x, and the boxes made of the segments
+#    [[x0,x0], [x0, g(x0)], [g(x0), g(x0)], [g(x0), g(g(x0))], ...
+#    """
+#    points = IterateList2D(g, init, N, args)
+#    matplotlib.pyplot.scatter(points[:,0], points[:,1], s=0.1, color='darkblue')
+
--- 22555/shapes.md
+++ 22555/shapes.md
@@ -1,3 +1,10 @@
 # Shapes #
 ## Equi-areal map ##
+Let L be a linear transformation.
+If det(L) = 1 then it is an area-preserving map.
+Suppose m ~ Lebesgue measure.
+Area-preserving is equivalent to a measure-preserving map
+i.e. m(L^{-1}}) = m(L).
 
+## Arnold's Cat Map ##
+Use Arnold's Cat Map to produce exotic shapes
